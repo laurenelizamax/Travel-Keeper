35d967dd988469ccd3ef7318a31bb293a033d8e0
lauren maxwell <laurenelizamax@icloud.com>
2019-11-10 12:12:24 -0600

    finally got it flexing right

--- src/components/trips/TripDetails.js
+++ src/components/trips/TripDetails.js
@@ -5,7 +5,7 @@ import AddLocation from "./AddLocation"
 import AddTravelers from "./AddTravelers"
 import EditTripForm from "./EditTripForm"
 import EditTravelersForm from "./EditTravelersForm"
-import "./Trip.css"
+import "./TripList.css"
 
 class TripDetails extends Component {
 
@@ -96,42 +96,41 @@ class TripDetails extends Component {
 
                 <AddLocation {...this.props} getData={this.getData} />
 
-                <div className="card">
-                    <div className="card-content">
-                        <div>
-                            <h4><strong>Title: </strong>{this.state.trip.title}</h4>
-                            <p><strong>Start Date: </strong>{this.state.trip.startDate}</p>
-                            <p><strong>End Date: </strong>{this.state.trip.endDate}</p>
-                            <p><strong>Notes:</strong> {this.state.trip.notes}</p>
-                            <EditTripForm {...this.props} getData={this.getData} />
+                <div className="container-cards">
+                    <div className="card">
+                        <div className="card-content">
+                            <div>
+                                <h4><strong>Title: </strong>{this.state.trip.title}</h4>
+                                <p><strong>Start Date: </strong>{this.state.trip.startDate}</p>
+                                <p><strong>End Date: </strong>{this.state.trip.endDate}</p>
+                                <p><strong>Notes:</strong> {this.state.trip.notes}</p>
+                                <EditTripForm {...this.props} getData={this.getData} />
+                            </div>
+                            {this.state.fellowTravelers.map(fellowTraveler =>
+                                <div key={fellowTraveler.id}>
+                                    <p><strong>Fellow Traveler: </strong>{fellowTraveler.travelerName}</p>
+                                    <EditTravelersForm fellowTravelerId={fellowTraveler.id} {...this.props} getData={this.getData} />
+                                    <button className="deleteButton" type="button" onClick={() =>
+                                        this.deleteTraveler(fellowTraveler.id)}>Delete Traveler</button>
+                                </div>
+                            )}
                         </div>
-                        {this.state.fellowTravelers.map(fellowTraveler =>
-                            <div key={fellowTraveler.id}>
-                                <p><strong>Fellow Traveler: </strong>{fellowTraveler.travelerName}</p>
-                                <EditTravelersForm fellowTravelerId={fellowTraveler.id} {...this.props} getData={this.getData} />
-                                <button className="deleteButton" type="button" onClick={() =>
-                                    this.deleteTraveler(fellowTraveler.id)}>Delete Traveler</button>
-                            </div>
-                        )}
-                   </div>
+                    </div>
                 </div>
 
-                <div className="card">
-                    <div className="card-content">
-                        {this.state.places.map(place =>
-                            <div key={place.id}>
-                                <PlaceCard
-                                    place={place}
-                                    placeId={place.id}
-                                    tripId={this.state.trip.id}
-                                    getData={this.getData}
-                                    deleteLocation={this.deleteLocation}
-                                />
-                            </div>
-                        )}
+                <div className="container-cards">
+                    {this.state.places.map(place =>
+                        // <div key={place.id}>
+                        <PlaceCard
+                            key={place.id}
+                            place={place}
+                            placeId={place.id}
+                            tripId={this.state.trip.id}
+                            getData={this.getData}
+                            deleteLocation={this.deleteLocation}
+                        />
+                    )}
                 </div>
-                </div>
-
             </>
         )
     }
